829acfe5bd1879a13eb008a3eb1d8e28015bb86e
maitabe <martushb@gmail.com>
2017-01-11 14:58:55 +0200

    duck face

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -1,8 +1,22 @@
-app.controller('MainCtrl', ['$scope', function($scope){
+app.controller('MainCtrl', ['$scope','$http', function($scope, $http){
 
 
+	function getData() {
+		return $http.get('/whatever').then(function(response, err) {
+			if (err) {console.log(err)};
+			// console.log(response.data.list);
 
+		});
+	}
+	// getData();
 
+	function getHistory() {
+		return $http.get('/history').then(function(response, err) {
+			if (err) {console.log(err)};
+			console.log(response);
 
+		});
+	}
 
+	getHistory();
 }]);
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
+var request = require('request');
+var fs = require('fs');
 
 mongoose.connect(process.env.MONGOLAB_ROSE_URI || 'mongodb://localhost/fanco');
 
@@ -19,8 +21,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 // app.use('/users', users); ********** uncomment to add user registration
 
+app.get('/whatever', function(req, res){
+
+	request('http://api.openweathermap.org/data/2.5/forecast/daily?q=Boston&APPID=eae18de7d92e5fa1893eeb187956805f&cnt=16', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body);
+		// var myReadStream = fs.createReadStream((body.data), { encoding: 'utf8' });
+		// var myWriteStream = fs.createWriteStream('data/forecast.json');
+		// myReadStream.pipe(myWriteStream);
+		fs.writeFile('data/forecast.json', body, 'utf8', function (err) {
+	  if (err) return console.log(err);
+	  // console.log();
+		});
+	    res.send(body);
+	  }
+	});
+});
+
+
+app.get('/history', function(req, res){
+
+	request('http://api.worldweatheronline.com/premium/v1/past-weather.ashx?q=Boston&format=json&date=2015-08-01&enddate=2016-05-30&key=22b79bd1845840e6a68110850171101&tp=24', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body);
+		fs.writeFile('data/history.json', body, 'utf8', function (err) {
+	  if (err) return console.log(err);
+
+		});
+	    res.send(body);
+	  }
+	});
+});
+
 var port = process.env.PORT || '7000';
 
 app.listen(port, function () {
   console.log('ok! On: localhost:' + port);
 });
+
+
