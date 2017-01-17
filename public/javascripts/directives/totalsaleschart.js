072b56a593a1f33d92ea3b48bcd1b7e218508341
benjkn <benj.kn@gmail.com>
2017-01-17 09:12:12 +0200

    wasnt working before

--- public/javascripts/directives/totalsaleschart.js
+++ public/javascripts/directives/totalsaleschart.js
@@ -5,184 +5,182 @@ app.directive("linearChart", [ 'sales', function(sales) {
     $scope.salesData = [];
     $scope.weatherData = [];
 
-		sales.getSales().then(function(response){
+		sales.getSales(1).then(function(response){
       $scope.salesData = response.data;
 
       sales.getRawHistory().then(function(response){
-      $scope.weatherData = response.data;
-      weatherData = $scope.weatherData.splice(0,358);
-      console.log(response);
-      console.log(weatherData);
+        $scope.weatherData = response.data;
+        weatherData = $scope.weatherData.splice(0,358);
+        // console.log(response); //// response.data shows array of 7 items! why??
+        // console.log(weatherData);
 
 
+    		//Nest + Rollup for Total Sales
+        var totalData = d3.nest()
+  			.key(function(d){ return (d.WeekOf); }).sortKeys(d3.ascending)
+  			.rollup(function(d){
+  				return d3.sum(d, function(g){
+  				  return g.SalesUnits;
+  				});
+  			}).entries($scope.salesData);
 
 
-  		//Nest + Rollup for Total Sales
-      var totalData = d3.nest()
-			.key(function(d){ return (d.WeekOf); }).sortKeys(d3.ascending)
-			.rollup(function(d){
-				return d3.sum(d, function(g){
-				  return g.SalesUnits;
-				});
-			}).entries($scope.salesData);
+    		// parse the dates!
+        var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+        for ( i=0; i<totalData.length; i++) {
+          totalData[i].key = parseDate(totalData[i].key);
+    		}
+
+         weatherData.forEach(function(d){
+          d.date = parseDate(d.date);
+          d.maxtempC = +d.maxtempC;
+        })
+        // console.log(weatherData);
 
 
-  		// parse the dates!
-      var parseDate = d3.time.format("%Y-%m-%d").parse;
+        // for ( i=0; i<$scope.weatherData.length; i++) {
+        //   $scope.weatherData[i].date = parseDate($scope.weatherData[i].date);
+    		// }
 
-      for ( i=0; i<totalData.length; i++) {
-        totalData[i].key = parseDate(totalData[i].key);
-  		}
-
-       weatherData.forEach(function(d){
-        d.date = parseDate(d.date);
-        d.maxtempC = +d.maxtempC;
-      })
-        console.log(weatherData);
+        // console.log($scope.weatherData);
 
 
-      // for ( i=0; i<$scope.weatherData.length; i++) {
-      //   $scope.weatherData[i].date = parseDate($scope.weatherData[i].date);
-  		// }
+        // Declare height and width variables(pixels)
+        var height = 300;
+        var width = 800;
+        // total = 0;
+        // Work out extremes
+        var maxSales = d3.max(totalData,function(d,i){
+          return d.values;
+        });
 
-      // console.log($scope.weatherData);
+        var maxHigh = d3.max(weatherData, function(d,i){
+          return d.maxtempC;
+        });
 
+        var minHigh = d3.min(weatherData, function(d,i){
+          return d.maxtempC;
+        });
 
-      // Declare height and width variables(pixels)
-      var height = 300;
-      var width = 800;
-      // total = 0;
-      // Work out extremes
-      var maxSales = d3.max(totalData,function(d,i){
-        return d.values;
-      });
+        // console.log(maxHigh);
+        // console.log(minHigh);
 
-      var maxHigh = d3.max(weatherData, function(d,i){
-        return d.maxtempC;
-      });
+        // find max temp day
+        // for ( i=0; i<weatherData.length; i++) {
+        //   if (weatherData[i].maxtempC === maxHigh){
+        //     console.log(weatherData[i]);
+        //   }
+        // }
 
-      var minHigh = d3.min(weatherData, function(d,i){
-        return d.maxtempC;
-      });
+        var minDate = d3.min(totalData,function(d){ return d.key; });
+        var maxDate = d3.max(totalData, function(d){ return d.key; });
 
-      console.log(maxHigh);
-      console.log(minHigh);
+        var minDate2 = d3.min($scope.weatherData,function(d){ return d.date; });
+        var maxDate2 = d3.max($scope.weatherData, function(d){ return d.date; });
+        // console.log(minDate);
+        // console.log(minDate2);
+        // console.log("Max Sales for a day is: " + maxSales);
 
-      // find max temp day
-      // for ( i=0; i<weatherData.length; i++) {
-      //   if (weatherData[i].maxtempC === maxHigh){
-      //     console.log(weatherData[i]);
-      //   }
-      // }
-
-      var minDate = d3.min(totalData,function(d){ return d.key; });
-      var maxDate = d3.max(totalData, function(d){ return d.key; });
-
-      var minDate2 = d3.min($scope.weatherData,function(d){ return d.date; });
-      var maxDate2 = d3.max($scope.weatherData, function(d){ return d.date; });
-      console.log(minDate);
-      console.log(minDate2);
-      // console.log("Max Sales for a day is: " + maxSales);
-
-      // find max value day
-      for ( i=0; i<totalData.length; i++) {
-        if (totalData[i].values === maxSales){
-          // console.log("most shit sold on " + totalData[i].key);
+        // find max value day
+        for ( i=0; i<totalData.length; i++) {
+          if (totalData[i].values === maxSales){
+            // console.log("most shit sold on " + totalData[i].key);
+          }
         }
-      }
 
-      var minDate = d3.min(totalData,function(d){ return d.key; });
-      var maxDate = d3.max(totalData, function(d){ return d.key; });
-      // console.log("minDate is " + minDate);
-      // console.log("maxDate is " + maxDate);
-
-
-			// Declare Scales + axes (don't forget to invert Y range)
-      var y = d3.scale.linear()
-        .domain([0,maxSales])
-        .range([height,0]);
-
-      var y2 = d3.scale.linear()
-        .domain([-15, maxHigh])
-        .range([height,0])
-
-      var x = d3.time.scale()
-        .domain([minDate,maxDate])
-        .range([0,width]);
-
-      var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-      var yAxis = d3.svg.axis()
-        .scale(y)
-        .ticks(5)
-        .orient("left");
-
-      var yAxis2 = d3.svg.axis()
-        .scale(y2)
-        .ticks(12)
-        .orient("right");
+        var minDate = d3.min(totalData,function(d){ return d.key; });
+        var maxDate = d3.max(totalData, function(d){ return d.key; });
+        // console.log("minDate is " + minDate);
+        // console.log("maxDate is " + maxDate);
 
 
-			var svg = d3.select($el[0]).append("svg")
-      	.attr("width", "100%")
-    		.attr("height", height + 100)
-    		.attr("style", "background: #f4f4f4");
+  			// Declare Scales + axes (don't forget to invert Y range)
+        var y = d3.scale.linear()
+          .domain([0,maxSales])
+          .range([height,0]);
 
-		  // Declare margin object (adds buffer)
-      var margin = {left:80,right:50,top:40,bottom:0};
+        var y2 = d3.scale.linear()
+          .domain([-15, maxHigh])
+          .range([height,0])
 
-      // Add all elements into group
-      var chartGroup = svg.append("g")
-        .attr("transform", "translate("+margin.left+","+margin.top+")");
+        var x = d3.time.scale()
+          .domain([minDate,maxDate])
+          .range([0,width]);
 
-		  var line = d3.svg.line()
-        .x(function(d){ return x(d.key); })
-        .y(function(d){ return y(d.values); })
-        .interpolate("cardinal");
+        var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
 
-      var line2 = d3.svg.line()
-        .x(function(d){ return x(d.date); })
-        .y(function(d){ return y2(d.maxtempC); })
-        .interpolate("cardinal");
+        var yAxis = d3.svg.axis()
+          .scale(y)
+          .ticks(5)
+          .orient("left");
 
-      // Finally add line; Append the path to group; run line generator on data
-      chartGroup.append("path").attr("d",line(totalData));
-      chartGroup.append("path").attr("d",line2(weatherData));
+        var yAxis2 = d3.svg.axis()
+          .scale(y2)
+          .ticks(12)
+          .orient("right");
 
-      // Add axes to group (shift x-axis down)
-      chartGroup.append("g").attr("class", "x axis")
-      .attr("transform", "translate(0, "+height+")").call(xAxis);
-      chartGroup.append("g").attr("class", "y axis").call(yAxis);
 
-      // temp axis
-      chartGroup.append("g").attr("class", "y2 axis")
-      .attr("transform", "translate("+width+",0)").call(yAxis2);
+  			var svg = d3.select($el[0]).append("svg")
+        	.attr("width", "100%")
+      		.attr("height", height + 100)
+      		.attr("style", "background: #f4f4f4");
 
-      // circles
-      chartGroup.selectAll("circle")
-        .data(totalData)
-        .enter().append("circle")
-          .attr("class",function(d,i){ return "grp"+i; })
-          .attr("cx",function(d,i){ return x(d.key); })
-          .attr("cy",function(d,i){ return y(d.values); })
-          .attr("r","2");
+  		  // Declare margin object (adds buffer)
+        var margin = {left:80,right:50,top:40,bottom:0};
 
-      // chartGroup.selectAll("circle")
-      //   .data(weatherData)
-      //   .enter().append("circle")
-      //     .attr("class",function(d,i){ return "temp"+i; })
-      //     .attr("cx",function(d,i){ return x(d.date); })
-      //     .attr("cy",function(d,i){ return y(d.maxtempC); })
-      //     .attr("r","2");
+        // Add all elements into group
+        var chartGroup = svg.append("g")
+          .attr("transform", "translate("+margin.left+","+margin.top+")");
+
+  		  var line = d3.svg.line()
+          .x(function(d){ return x(d.key); })
+          .y(function(d){ return y(d.values); })
+          .interpolate("cardinal");
+
+        var line2 = d3.svg.line()
+          .x(function(d){ return x(d.date); })
+          .y(function(d){ return y2(d.maxtempC); })
+          .interpolate("cardinal");
+
+        // Finally add line; Append the path to group; run line generator on data
+        chartGroup.append("path").attr("d",line(totalData));
+        chartGroup.append("path").attr("d",line2(weatherData));
+
+        // Add axes to group (shift x-axis down)
+        chartGroup.append("g").attr("class", "x axis")
+        .attr("transform", "translate(0, "+height+")").call(xAxis);
+        chartGroup.append("g").attr("class", "y axis").call(yAxis);
+
+        // temp axis
+        chartGroup.append("g").attr("class", "y2 axis")
+        .attr("transform", "translate("+width+",0)").call(yAxis2);
+
+        // circles
+        chartGroup.selectAll("circle")
+          .data(totalData)
+          .enter().append("circle")
+            .attr("class",function(d,i){ return "grp"+i; })
+            .attr("cx",function(d,i){ return x(d.key); })
+            .attr("cy",function(d,i){ return y(d.values); })
+            .attr("r","2");
+
+        // chartGroup.selectAll("circle")
+        //   .data(weatherData)
+        //   .enter().append("circle")
+        //     .attr("class",function(d,i){ return "temp"+i; })
+        //     .attr("cx",function(d,i){ return x(d.date); })
+        //     .attr("cy",function(d,i){ return y(d.maxtempC); })
+        //     .attr("r","2");
       });
 		});
 	};
 
 	return {
 		restrict: "EA",
-		template: '<div class="fuck"</div>',
+		template: '<div class="fuck"></div>',
 		replace: true,
 		link: link
 	};
--- public/javascripts/service/salesService.js
+++ public/javascripts/service/salesService.js
@@ -12,9 +12,9 @@ app.factory('sales', ['$http', function($http) {
 
       //every variable is true WHEN there IS the attribute (default is no attribute and means -->ALL) && that attribue is NOT ALL ===> then it is custom
       //if a true then CUSTOM PRODUCTS
-      var a = (prefs.prods.length && prefs.prods.length !== 3);
+      var a = (prefs.prods && prefs.prods.length !== 3);
       //if b true then CUSTOM NEIGHBORHOODS
-      var b = (prefs.neighborhoods.length && prefs.neighborhoods.length !== 8);
+      var b = (prefs.neighborhoods && prefs.neighborhoods.length !== 8);
       //if c true then CUSTOM CHANNELS
       var c = (prefs.channel && prefs.channel !== "Both");
       //if d true then CUSTOM DATES
@@ -25,131 +25,161 @@ app.factory('sales', ['$http', function($http) {
 
       if (!(a || b || c || d)) {
       //(0 custom, 4 'all')
+        console.log('we are in situation ALL is ALL!');
         return $http.get('/sales');
       } else if (a && b && c && d) {
       //(4 custom, 0 'all')
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a && c && d) {
       //(3 custom, 1 'all') - Customized: Products AND Channels AND Dates ------- ALL: Neighborhoods
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (b && c && d) {
       //(3 custom, 1 'all') - Customized: Neighborhoods AND Channels AND Dates ------- ALL: Products
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a && b && d) {
       //(3 custom, 1 'all') - Customized: Products AND Neighborhoods AND Dates ------- ALL: Channels
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a && b && c) {
       //(3 custom, 1 'all') - Customized: Products AND Neighborhoods AND Channels ------- ALL: Dates
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a && b) {
       //(2 custom, 2 'all') - Customized: Products AND Neighborhoods ------- ALL: Dates, Channels
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a && c) {
       //(2 custom, 2 'all') - Customized: Products AND Channels ------- ALL: Neighborhoods AND Dates
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a && d) {
       //(2 custom, 2 'all') - Customized: Products AND Dates ------- ALL: Neighborhoods AND Channels
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (b && c) {
       //(2 custom, 2 'all') - Customized: Neighborhoods AND Channels ------- ALL: Products AND Dates
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (b && d) {
       //(2 custom, 2 'all') - Customized: Neighborhoods AND Dates ------- ALL: Products AND Channels
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (c && d) {
       //(2 custom, 2 'all') - Customized: Channels AND Dates ------- ALL: Neighborhoods AND Products
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (a) {
       //(1 custom, 3 'all') - Customized: Products ------- ALL: Neighborhoods AND  Channels AND Dates
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (b) {
       //(1 custom, 3 'all') - Customized: Neighborhoods ------- ALL: Products AND Channels AND Dates
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (c) {
       //(1 custom, 3 'all') - Customized: Channels ------- ALL: Neighborhoods AND Products AND Dates
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else if (d) {
       //(1 custom, 3 'all') - Customized: Dates ------- ALL: Neighborhoods AND Products AND Channels
 
         //Add the special params I am sending
-        return $http.get('/sales' + );
+        console.log('we are inside a specific situation:');
+        console.log('a is: ' + a + ', b is: ' + b + ', c is: ' + c + ' and d is: ' + d);
+        return $http.get('/sales');
 
       } else {
       //(0 custom, 4 'all') - Covered it 1st
-        console.log ('I should NEVER get in here! :)')
+        console.log ('I should NEVER get in here! :)');
       }
+
   	},
 
 
-    //finally weatherHistory is an array of 52 objects with the first day of each week in 1433019600000 format and the average temp!
-    //this date format is easily comparable and can be turned into a date by new Date(1436648400000)
-
     getRawHistory: function(){
       return $http.get('/history');
-    },
-
-    getHistory: function() {
-      return $http.get('/history').then(function (history) {
-        salesService.temporaryArray = history.data;
-        for (i=0; i<salesService.temporaryArray.length; i++) {
-          if (i%7 === 0 && i>0) {
-            var week = 1433019600000+(salesService.weatherHistory.length)*7*86400000;
-            var temperature = (Math.round(salesService.count*100/14))/100;
-            var obj = {
-              temperature: temperature,
-              WeekOf: week
-            };
-            salesService.count = 0;
-            salesService.weatherHistory.push(obj);
-          }
-          salesService.count = salesService.count+parseInt(salesService.temporaryArray[i].maxtempC, 10)+parseInt(salesService.temporaryArray[i].mintempC, 10);
-        }
-        // console.log(salesService.weatherHistory);
-        // console.log(salesService.weatherHistory.length);
-      });
     }
+
+    //not used now
+    // getHistory: function() {
+    //   return $http.get('/history').then(function (history) {
+    //     salesService.temporaryArray = history.data;
+    //     for (i=0; i<salesService.temporaryArray.length; i++) {
+    //       if (i%7 === 0 && i>0) {
+    //         var week = 1433019600000+(salesService.weatherHistory.length)*7*86400000;
+    //         var temperature = (Math.round(salesService.count*100/14))/100;
+    //         var obj = {
+    //           temperature: temperature,
+    //           WeekOf: week
+    //         };
+    //         salesService.count = 0;
+    //         salesService.weatherHistory.push(obj);
+    //       }
+    //       salesService.count = salesService.count+parseInt(salesService.temporaryArray[i].maxtempC, 10)+parseInt(salesService.temporaryArray[i].mintempC, 10);
+    //     }
+    //     console.log(salesService.weatherHistory);
+    //     console.log(salesService.weatherHistory.length);
+    //   });
+    // }
 
 
   };
