0e5922d3714d3e2c430d387eb5472eaacc990949
benjkn <benj.kn@gmail.com>
2017-01-25 16:47:25 +0200

    heroku more more checks

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -11,90 +11,6 @@ app.controller('MainCtrl', ['$scope','sales', function($scope, sales){
 	// var theNeighborhoods = document.getElementsByClassName('neighList');
 	var theDates = document.getElementsByClassName('datesPick');
 
-/*	$scope.pickProducts = function (prod) {
-		var prodcounter = 0;
-		var productSelected = [];
-		for (i=0; i<theProds.length-1; i++) {
-			if (theProds[i].checked) {
-				prodcounter += 1
-				productSelected.push($scope.products[i])
-			}
-		}
-
-		if (prod.$index === 3) {
-			var productSelected = [];
-			if (theProds[3].checked) {
-				for (i=0; i<theProds.length-1; i++) {
-					theProds[i].checked = theProds[3].checked
-					productSelected.push($scope.products[i])
-				}
-			} else {
-				for (i=0; i<theProds.length-1; i++) {
-					theProds[i].checked = theProds[3].checked
-				}
-			}
-		} else if (prodcounter === 3) {
-			theProds[3].checked = true
-		} else {
-			theProds[3].checked = false
-		}
-		// RESULT:
-		$scope.preferences.prods = productSelected;
-		console.log($scope.preferences);
-		sales.getSales($scope.preferences).then(function (data) {
-			console.log (data.data);
-		});
-	};
-
-	$scope.pickChannel = function () {
-		for (j=0; j<$scope.channels.length; j++) {
-			if (theChannel[j].checked) {
-				var channelSelected = theChannel[j].value;
-			}
-		}
-		// RESULT:
-		$scope.preferences.channel = channelSelected;
-		console.log($scope.preferences);
-		sales.getSales($scope.preferences).then(function (data) {
-			console.log (data.data);
-		});
-	};
-
-	$scope.pickNeighborhoods = function (neigh) {
-		var neighcounter = 0;
-		var neighborhoodSelected = [];
-
-		for (y=0; y<theNeighborhoods.length-1; y++) {
-			if (theNeighborhoods[y].checked) {
-				neighcounter +=1
-				neighborhoodSelected.push($scope.neighborhoods[y])
-			}
-		}
-
-		if (neigh.$index === 8) {
-			neighborhoodSelected = []
-			if (theNeighborhoods[8].checked) {
-				for (y=0; y<theNeighborhoods.length-1; y++) {
-					theNeighborhoods[y].checked = theNeighborhoods[8].checked
-					neighborhoodSelected.push($scope.neighborhoods[y]);
-				}
-			} else {
-				for (y=0; y<theNeighborhoods.length-1; y++) {
-					theNeighborhoods[y].checked = theNeighborhoods[8].checked
-				}
-			}
-		} else if (neighcounter === 8) {
-			theNeighborhoods[8].checked = true;
-		} else {
-			theNeighborhoods[8].checked = false;
-		}
-		// RESULT:
-		$scope.preferences.neighborhoods = neighborhoodSelected;
-		console.log($scope.preferences);
-		sales.getSales($scope.preferences).then(function (data) {
-			console.log (data.data);
-		});
-	};*/
 
 	$scope.pickDate = function () {
 		if (theDates[0].checked) {
@@ -109,7 +25,7 @@ app.controller('MainCtrl', ['$scope','sales', function($scope, sales){
 		}
 	};
 
-	$scope.getDateDetails = function (x, y) {
+	/*$scope.getDateDetails = function (x, y) {
 
 		startingDate = max(x, 31/5/2015)
 		endingDate = min(1/6/2016, y)
@@ -121,6 +37,6 @@ app.controller('MainCtrl', ['$scope','sales', function($scope, sales){
 		sales.getSalesByDate(startingDate, endingDate).then(function (data) {
 			console.log (data.data);
 		});
-	}
+	}*/
 
 }]);
